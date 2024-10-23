5d353ecd16153be69f38c72d6b8e9e6934af73eb
David Dias <david.vidt@gmail.com>
2024-10-23 16:59:55 -0300

    app simples feito

---
+++ app/src/main/java/com/edu/unicid/ecosaber/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.edu.unicid.ecosaber;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){start(v);}
+        });
+
+    }
+
+    public void start(View view){
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+
+    }
+
+}
